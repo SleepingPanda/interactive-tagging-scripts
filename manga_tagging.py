28e2c50b08e7f3ee31c5a1bb8b601e8b80afdb37
SleepingPanda <5846561+SleepingPanda@users.noreply.github.com>
2024-01-19 08:33:19 -0400

    Update manga_tagging.py

--- manga_tagging.py
+++ manga_tagging.py
@@ -3,41 +3,80 @@ import subprocess
 import re
 from colorama import Fore, Style
 
+init()
+
 def list_directories_and_files():
-    directories = []
-    files = []
+    """List directories and CBZ files in the current working directory.
+
+    Returns:
+        Tuple: A tuple containing two lists - dir_list and file_list.
+               The 'dir_list' list contains the names of directories in the current directory,
+               and the 'file_list' list contains the names of CBZ files.
+    """
+
+    dir_list = []
+    file_list = []
     for item in os.listdir('.'):
         if os.path.isdir(item):
-            directories.append(item)
+            dir_list.append(item)
         elif item.endswith(".cbz"):
-            files.append(item)
-    return directories, files
+            file_list.append(item)
+    return dir_list, file_list
 
 def choose_directory_or_file(directories, files):
+    """Allow the user to choose a directory or CBZ file to work on interactively.
+
+    Args:
+        directories (list): A list of directory names.
+        files (list): A list of CBZ file names.
+
+    Returns:
+        Tuple: A tuple containing two items - selected_directory and selected_file.
+               selected_directory (str): The selected directory name, or None if no directory is selected.
+               selected_file (str): The selected CBZ file name, or None if no CBZ file is selected.
+    """
     while True:
         print(Fore.YELLOW + "Directories:")
-        for i, directory in enumerate(directories, 1):
-            print(Fore.RED + f"{i}." + Fore.GREEN + f" {directory}")
+        for i, dir_list in enumerate(directories, 1):
+            print(Fore.RED + f"{i}." + Fore.GREEN + f" {dir_list}")
         print(Fore.YELLOW + "Files:")
-        for i, file in enumerate(files, 1):
-             print(Fore.RED + f"{i+len(directories)}." + Fore.BLUE + f" {file}")
+        for i, file_list in enumerate(files, 1):
+            print(Fore.RED + f"{i+len(directories)}." + Fore.BLUE + f" {file_list}")
         choice = input(Fore.RESET + "Enter the number of the directory or file you want to work on or type 'exit' to quit: ")
         if choice.lower() == 'exit':
             return None, None
-        elif not choice.isdigit() or int(choice) < 1 or int(choice) > len(directories) + len(files):
-            print("Invalid choice. Please enter a valid number.")
-        else:
+        try:
             choice = int(choice)
+            if choice < 1 or choice > len(directories) + len(files):
+                raise ValueError
             if choice <= len(directories):
                 return directories[choice - 1], None
             else:
                 return None, files[choice - len(directories) - 1]
+        except ValueError:
+            print("Invalid choice. Please enter a valid number.")
 
 def get_directory_path(directory):
+    """Get the absolute path of a directory.
+
+    Args:
+        directory (str): The name of the directory.
+
+    Returns:
+        str: The absolute path of the directory.
+    """
     return os.path.abspath(directory)
 
-def check_directory_exists(directory_path):
-    return os.path.exists(directory_path)
+def check_directory_exists(dir_path):
+    """Check if a directory exists.
+
+    Args:
+        directory_path (str): The absolute path of the directory.
+
+    Returns:
+        bool: True if the directory exists, False otherwise.
+    """
+    return os.path.exists(dir_path)
 
 def process_cbz_files(dir_path, specific_file=None):
     """Process CBZ files in a directory and update their metadata interactively.
@@ -72,6 +111,82 @@ def process_cbz_files(dir_path, specific_file=None):
 
     print("Job completed.")
 
+def get_metadata_input():
+    """Prompt the user to input metadata fields for a CBZ file.
+
+    Returns:
+        dict: A dictionary containing metadata fields (year, month, day, title, comments).
+    """
+    metadata = {}
+    try:
+        year = input("Enter the year: ")
+        if year:
+            if not year.isnumeric() or not (1900 <= int(year) <= 2100):
+                raise ValueError
+            metadata['year'] = year
+        month = input("Enter the month: ")
+        if month:
+            if not month.isnumeric() or not (1 <= int(month) <= 12):
+                raise ValueError
+            metadata['month'] = month
+        day = input("Enter the day: ")
+        if day:
+            if not day.isnumeric() or not (1 <= int(day) <= 31):
+                raise ValueError
+            metadata['day'] = day
+        title = input("Enter the title: ")
+        if title:
+            metadata['title'] = clean_string(title)
+            volume = extract_volume_number(title)
+            if volume:
+                metadata['volume'] = volume
+        comments = input("Enter the comments: ")
+        if comments:
+            metadata['comments'] = clean_string(comments)
+        return metadata
+    except ValueError:
+        print("Invalid input. Please enter a valid value.")
+        return None
+
+def clean_string(string):
+    """Clean a string by replacing certain characters.
+
+    Args:
+        string (str): The string to be cleaned.
+
+    Returns:
+        str: The cleaned string.
+    """
+    return string.replace(',', '^,').replace('=', '^=').strip().replace('  ', ' ').replace('\n', '').replace('...', '…')
+
+def get_comictagger_command(metadata, file_path):
+    """Construct the ComicTagger command for updating metadata.
+
+    Args:
+        metadata (dict): A dictionary containing metadata fields.
+        file_path (str): The path to the CBZ file.
+
+    Returns:
+        list: A list representing the ComicTagger command.
+    """
+    # The comictagger command has the following format:
+    # comictagger -s -t CR --overwrite --metadata field1=value1,field2=value2,... file_path
+    # The -s flag means to write tags to the zip comment
+    # The -t CR flag means to use the ComicRack tag format
+    # The --overwrite flag means to overwrite existing tags
+    # The --metadata flag allows to specify the metadata fields and values to update
+    # The file_path is the path to the CBZ file
+    return [
+        "comictagger",
+        "-s",
+        "-t",
+        "CR",
+        "--overwrite",
+        "--metadata",
+        ','.join(f"{key}={value}" for key, value in metadata.items()),
+        file_path
+    ]
+
 def extract_volume_number(title):
     """Extract the volume number from a title.
 
@@ -89,18 +204,19 @@ def extract_volume_number(title):
         return match.group(1)
     return ""
 
-directories, files = list_directories_and_files()
-if len(directories) == 0 and len(files) == 0:
-    print("No directories or .cbz files found in the current working directory.")
-else:
-    selected_directory, selected_file = choose_directory_or_file(directories, files)
-    if selected_directory is None and selected_file is None:
-        print("Exiting the program.")
-    elif selected_directory is not None:
-        directory_path = get_directory_path(selected_directory)
-        if not check_directory_exists(directory_path):
-            print("Directory does not exist.")
-        else:
-            process_cbz_files(directory_path)
+if __name__ == "__main__":
+    directories, files = list_directories_and_files()
+    if len(directories) == 0 and len(files) == 0:
+        print("No directories or .cbz files found in the current working directory.")
     else:
-        process_cbz_files('.', selected_file)
+        selected_directory, selected_file = choose_directory_or_file(directories, files)
+        if selected_directory is None and selected_file is None:
+            print("Exiting the program.")
+        elif selected_directory is not None:
+            directory_path = get_directory_path(selected_directory)
+            if not check_directory_exists(directory_path):
+                print("Directory does not exist.")
+            else:
+                process_cbz_files(directory_path)
+        else:
+            process_cbz_files('.', selected_file)
